ac95857483d49cfa3ebfb7f229bb1eeb146f9bdb
Garrett Davidson <davidson.garrettm@gmail.com>
2014-11-30 11:31:15 -0500

    Initial Commit

---
+++ iOS Backup Forensics Toolkit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOS Backup Forensics Toolkit
+//
+//  Created by Garrett Davidson on 11/30/14.
+//  Copyright (c) 2014 Garrett Davidson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ iOS Backup Forensics ToolkitTests/iOS_Backup_Forensics_ToolkitTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_Backup_Forensics_ToolkitTests.swift
+//  iOS Backup Forensics ToolkitTests
+//
+//  Created by Garrett Davidson on 11/30/14.
+//  Copyright (c) 2014 Garrett Davidson. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class iOS_Backup_Forensics_ToolkitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
